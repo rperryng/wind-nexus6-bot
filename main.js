5470fa46d3bda081245686ca08449fc8cdb3eab6
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2014-12-16 15:50:31 -0500

    Validate whether or not the tweet is a promotional tweet or not

--- main.js
+++ main.js
@@ -4,27 +4,49 @@ var ONE_MINUTE_IN_MILLIS = 60000;
 
 var twitter = new Twit(config);
 
-setInterval(checkForTweet, ONE_MINUTE_IN_MILLIS);
-checkForTweet();
+setInterval(checkForPromotionalTweet, ONE_MINUTE_IN_MILLIS);
+checkForPromotionalTweet();
 
-function checkForTweet() {
+function checkForPromotionalTweet() {
 
   var endpoint = 'statuses/user_timeline';
   var params = {
     screen_name: 'WINDmobile',
-    count: 2,
+    count: 20,
     include_rts: false,
     exclude_replies: true
   };
 
 
   twitter.get(endpoint, params, function (err, data) {
+
+    console.log('got', data.length, 'results');
+
     if (err || !data) {
       return;
     }
 
     data.forEach(function (tweet, index) {
-      console.log(index + ' - ' + tweet.id + ': ' + tweet.text);
+//      console.log(index + ' - ' + tweet.id + ': ' + tweet.text);
+
+      var text = tweet.text;
+
+      var keyWords = ['5', 'min', '#Nexus6', '#BirthdayGift'];
+
+      var hasAllKeywords = keyWords.every(function (keyword) {
+        var isValid = text.indexOf(keyword) > -1;
+
+        if (!isValid) {
+          console.log('couldn\'t find', keyword);
+        }
+        return isValid;
+      });
+
+
+
+      console.log('text: ' + tweet.text);
+      console.log('isValid: ' + hasAllKeywords);
+      console.log();
     });
   });
 }
